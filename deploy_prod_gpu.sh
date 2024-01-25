d8cdbf8194dd33c45f9eead13fcb52646a6fd093
Ubuntu <ubuntu@ip-172-31-14-231.us-west-2.compute.internal>
2024-01-25 18:50:37 +0000

    Add mount to script

--- deploy_prod_gpu.sh
+++ deploy_prod_gpu.sh
@@ -1,4 +1,4 @@
 docker compose down
 CVAT_HOST=ec2-54-201-133-156.us-west-2.compute.amazonaws.com DOCKER_BUILDKIT=1 docker compose -f docker-compose.yml -f docker-compose.dev.yml -f components/serverless/docker-compose.serverless.yml up -d --build
-
+sudo mount -o iam_role,umask=0007,uid=1000 sewerai_prod
 
