ba57abe09c35acc7f4598081dd4ed93eafd4f4c5
Ubuntu <ubuntu@ip-172-31-14-231.us-west-2.compute.internal>
2024-06-05 20:26:12 +0000

    Check remote changes

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,4 @@
-docker-compose down
-CVAT_HOST=ec2-44-234-209-83.us-west-2.compute.amazonaws.com DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml -f docker-compose.dev.yml -f components/serverless/docker-compose.serverless.yml up -d --build
+docker compose down
+CVAT_HOST=ec2-18-246-40-254.us-west-2.compute.amazonaws.com DOCKER_BUILDKIT=1 docker compose -f docker-compose.yml -f docker-compose.dev.yml -f components/serverless/docker-compose.serverless.yml up -d --build
 
 
